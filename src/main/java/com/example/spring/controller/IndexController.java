e146e269175e3922a3d5f5223ff81d269fb66f75
2Doyoung <dlehdud1653@kakao.com>
2025-05-09 16:26:16 +0900

    role

--- src/main/java/com/example/spring/controller/IndexController.java
+++ src/main/java/com/example/spring/controller/IndexController.java
@@ -3,6 +3,8 @@ package com.example.spring.controller;
 import com.example.spring.model.User;
 import com.example.spring.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -58,4 +60,17 @@ public class IndexController {
         userRepository.save(user);
         return "redirect:/loginForm";
     }
+
+    @Secured("ROLE_ADMIN")
+    @GetMapping("/info")
+    public @ResponseBody String info() {
+        return "개인정보";
+    }
+
+    @PreAuthorize("hasRole('ROLE_MANAGER')") // ROLE_ 없이 "hasRole('ADMIN')" 같은 표현 사용. 권한뿐 아니라 매개변수 기반 조건도 가능.
+    @GetMapping("/data")
+    public @ResponseBody String data() {
+        return "데이터정보";
+    }
+
 }
